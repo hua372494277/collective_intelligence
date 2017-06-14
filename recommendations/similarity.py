67661d478ebceddd2d23f20f0601cfa45eaf1002
hua372494277 <huayongpan@gmail.com>
2017-06-14 16:35:36 +0800

    Add: Super Class of similatiry

--- recommendations/similarity.py
+++ recommendations/similarity.py
@@ -1,8 +1,10 @@
-class similarity(object):
+class Similarity(object):
+    # item1: dict
+    # {
+    #     'Lady in the Water': 2.5, 
+    #     'Snakes on a Plane': 3.5,
+    #     'Just My Luck': 3.0, 
+    #     ..............
+    # }
     def calculate_similarity(self, item1, item2):
         pass
-
-sim = similarity()
-sim.x = 1
-print sim.x
-
