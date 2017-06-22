8587843196cb22f7011ccc81c046c650044cc56d
hua372494277 <huayongpan@gmail.com>
2017-06-22 19:30:45 +0800

    Modify: add num and delete node

--- code.cpp
+++ code.cpp
@@ -4,10 +4,13 @@
 #include <iostream>
 #include <time.h>
 #include <bitset>
+#include <fstream>
 using namespace std;
 
 int squares[] = {0 , 1 , 4 , 9 , 16 , 25 , 36 , 49 , 64 , 81};
 bool primes[1459];
+
+ofstream myfile;
 
 bool isPrime(int n){
     if(n < 2){
@@ -36,6 +39,7 @@ struct num_List{
     unsigned long long num;
     struct num_List * next;
 };
+
 struct num_List * num_list_head;
 struct num_List * num_list_cur;
 
@@ -43,7 +47,7 @@ void initIsPrime(){
     for(int i = 0 ; i < 1459 ; ++i){
         primes[i] = isPrime(i);
     }
-    
+
     // initialize only one number in the chain
     combinations[1] = new struct item();
     combinations[1]->nums = new char();
@@ -51,7 +55,7 @@ void initIsPrime(){
     combinations[1]->sum = 1;
     combinations[1]->square = 1;
     struct item * pre = combinations[1];
-    
+
     for(int i = 1; i < 10; i ++){
         struct item * p = new struct item();
         p->nums = new char();
@@ -62,84 +66,27 @@ void initIsPrime(){
         pre->next = p;
         pre = p;
     }
-    
+
     num_list_head = new struct num_List();
     num_list_head->num = 0;
     num_list_head->next = NULL;
     num_list_cur = num_list_head;
 }
 
-void addOneNum(int time){
-    combinations[time] = new struct item();
-    combinations[time]->nums = new char[time];
-    combinations[time]->nums[0] = 1;
-    
-    struct item * lastChain = combinations[time-1];
-    struct item * pre = combinations[time];// current chain to generate new items
-    
-    while(lastChain->next){
-        lastChain = lastChain->next;
-        
-        //Time is from 1 - 18
-        //But nums is starting from 0
-        for(int i = 0; i <= lastChain->nums[time-2]; i ++){
-            if(time == 18 && (!primes[i + lastChain->sum]|| !primes[squares[i] + lastChain->square])){
-                continue;
-            }
-            struct item * p = new struct item();
-            
-            p->nums = new char[time];
-            // copy the high positions
-            //such as, from the last chain, item 1,
-            // generate 1*, then 1 should be copy to nums
-            for(int j = 0; j < time - 1; j ++){
-                p->nums[j] = lastChain->nums[j];
-            }
-            // generate the number on the lowest position
-            // this number could not be larger than the number on higher positions
-            p->nums[time - 1 ] = i;
-            p->sum = i + lastChain->sum;
-            p->square = squares[i] + lastChain->square;
-            p->next = NULL;
-            pre->next = p;
-            pre = p;
-        }
-    }
-}
-
-void cleanPreChain(int time){
-    struct item * cur = combinations[time];
-    struct item * pre = cur;
-    cur = cur->next;
-    
-    while(cur){
-        if(primes[cur->sum] && primes[cur->square]){
-            pre = cur;
-            cur = cur->next;
-            continue;
-        }else{
-            pre->next = cur->next;
-            delete [] cur->nums;
-            delete cur;
-            cur = pre->next;
-        }
-    }
-}
-
 int is_swap(char *str, int begin, int k){
     int i, flag;
-    
+
     for (i = begin, flag = 1; i < k; i ++) {
         if (str[i] == str[k]) {
             flag = 0;
             break;
         }
     }
-    
+
     return flag;
 }
 
-void swap(char *str, char a, char b)
+void swap(char *str, int a, int b)
 {
     char temp;
     temp = str[a];
@@ -149,10 +96,17 @@ void swap(char *str, char a, char b)
 
 void permutation_process(char *nums, int begin, int end, unsigned long long base) {
     int k;
-    
+
     if (begin == end - 1)
     {
         unsigned long long index = 0;
+        myfile << "Output: ";
+        for (int i = 0; i < end; i++){
+        	myfile << (int) nums[i] << ", ";
+        	cout << (int) nums[i] << ", ";
+        }
+        myfile << base << "\n";
+        cout << base << "\n";
         for(int i = end - 1; i > -1; i --){
             index += base * ((int) nums[i]);
             base *= 10;
@@ -178,11 +132,91 @@ void permutation_process(char *nums, int begin, int end, unsigned long long base
     }
 }
 
+void addAndDeleteOneNum(int time){
+	// initialize the head node
+	// the nums[0] saved how many digits in each numbers
+    combinations[time] = new struct item();
+    combinations[time]->nums = new char[time];
+    combinations[time]->nums[0] = time;
+    struct item * pre = combinations[time];// current chain to generate new items
+
+    struct item * preLastChain = combinations[time-1];
+    struct item * curLastChain = preLastChain->next;
+
+    while(curLastChain){
+        //Time is from 1 - 18
+        //But nums is starting from 0
+        for(int i = 0; i <= curLastChain->nums[time-2]; i ++){
+            if(time == 18 && (!primes[i + curLastChain->sum]|| !primes[squares[i] + curLastChain->square])){
+                continue;
+            }
+            struct item * p = new struct item();
+            p->nums = new char[time];
+            // copy the high positions
+            //such as, from the last chain, item 1,
+            // generate 1*, then 1 should be copy to nums
+            for(int j = 0; j < time - 1; j ++){
+                p->nums[j] = curLastChain->nums[j];
+                myfile << (int) p->nums[j] << ", ";
+                cout << int(p->nums[j])<< ",";
+            }
+            // generate the number on the lowest position
+            // this number could not be larger than the number on higher positions
+            p->nums[time - 1 ] = i;
+            myfile << (int) p->nums[time - 1] << "\n";
+            cout <<  (int)p->nums[time - 1] << "\n";
+            p->sum = i + curLastChain->sum;
+            p->square = squares[i] + curLastChain->square;
+            p->next = NULL;
+            pre->next = p;
+            pre = p;
+        }
+
+        // Deal with the nodes on last chain
+        // If it is not prime number
+        // Delete this node from last chain
+        if(primes[curLastChain->sum] && primes[curLastChain->square]){
+        	unsigned long long base = 1;
+			int len = time - 1;
+
+			for (int i = time - 2; i >= 0 && curLastChain->nums[i] == 0; i--) {
+				base *= 10;
+				len --;
+			}
+			permutation_process(curLastChain->nums, 0, len, base);
+		}
+		preLastChain->next = curLastChain->next;
+		delete [] curLastChain->nums;
+		delete curLastChain;
+		curLastChain = preLastChain->next;
+    }
+}
+
+void cleanPreChain(int time){
+    struct item * cur = combinations[time];
+    struct item * pre = cur;
+    cur = cur->next;
+
+    while(cur){
+        if(primes[cur->sum] && primes[cur->square]){
+            pre = cur;
+            cur = cur->next;
+            continue;
+        }else{
+            pre->next = cur->next;
+            delete [] cur->nums;
+            delete cur;
+            cur = pre->next;
+        }
+    }
+}
+
+
 
 void setBitBaseNums(char * nums, int time){
     unsigned long long base = 1;
     int len = time;
-    
+
     for (int i = time - 1; i >= 0 && nums[i] == 0; i--) {
         base *= 10;
         len --;
@@ -194,7 +228,7 @@ void setBitForNum(int time){
     struct item * cur = combinations[time];
     struct item * pre = cur;
     cur = cur->next;
-    
+
     while(cur){
         setBitBaseNums(cur->nums, time);
         pre->next = cur->next;
@@ -202,27 +236,29 @@ void setBitForNum(int time){
         delete cur;
         cur = pre->next;
     }
-    
+
     delete [] combinations[time]->nums;
     delete combinations[time];
 }
 
 void combination(){
     for(int time = 2; time < 19; time++){
-        addOneNum(time);
-        cleanPreChain(time - 1);
-        setBitForNum(time - 1);
+    	addAndDeleteOneNum(time);
+        //cleanPreChain(time - 1);
+        //setBitForNum(time - 1);
     }
-    cleanPreChain(18);
+//    cleanPreChain(18);
     setBitForNum(18);
 }
 
 
 int main()
 {
-    unsigned long long x, y;
-    cin >> x >> y;
-    
+	myfile.open("C:\\computer_science_y\\version_control\\collective_intelligence\\example.csv");
+//	myfile << "Writing this to a file.\n";
+
+    unsigned long long x = 1, y = 100000000;
+    //cin >> x >> y;
     unsigned long long count = 0;
     initIsPrime();
     combination();
@@ -237,6 +273,7 @@ int main()
         p = p->next;
     }
     cout << count;
+    myfile.close();
 //    num_list.set(n);
     //    unsigned long count = 0;
 //    for(int time = 2; time < 19; time++){
@@ -247,8 +284,8 @@ int main()
 //        }
 //    }
 //    cout << count;
-    
-    
+
+
     //	bitset3.reset();
     //	bitset3.set(2);
     //	bitset3.set(4);
