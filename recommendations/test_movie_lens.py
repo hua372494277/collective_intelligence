998ff5800cc7d60e23f201b9061f9f48a785ac40
hua372494277 <huayongpan@gmail.com>
2017-07-03 23:15:23 +0800

    Add: test recommendations based on two ways - based users or items

--- recommendations/test_movie_lens.py
+++ recommendations/test_movie_lens.py
@@ -1,4 +1,5 @@
-from recommendations import Recommendation, transform_data
+from recommendations import Recommendation
+from recommend_base_items import RecommendationBaseItems
 
 def load_movies_lens(path=""):
     # get the info of movies
@@ -18,28 +19,35 @@ def load_movies_lens(path=""):
 if __name__ == '__main__':
     prefs = load_movies_lens("C:\\Users\\huab\\Downloads\\ml-100k\\ml-100k")
     recommend = Recommendation(prefs)
+    rec_base_item = RecommendationBaseItems(prefs)
+    
     import time
-
     start = time.clock()
-    print recommend.getRecommendations('87')[0:30]
+    print recommend.get_recommendations('87')[0:30]
     elapsed = (time.clock() - start)
     print elapsed
 
-    recommand_elements = Recommendation(transform_data(prefs))
-
-    recommand_elements.set_similarity_calculator("sim_distance")
-
-    recommand_elements.update_sim_elememts()
+    #print recommend_elements
     start = time.clock()
-    print recommand_elements.getRecommendedElements(prefs, '1633')[0:30]
+    print rec_base_item.get_recommended_items("87", update = True, num = 50)[0:30]
+    elapsed = (time.clock() - start)
+    print elapsed
+    
+    rec_base_item.set_similarity_calculator("sim_distance")
+    start = time.clock()
+    print rec_base_item.get_recommended_items("87", update = True, num = 50)[0:30]
+    elapsed = (time.clock() - start)
+    print elapsed
+    
+    print "recommend Ripe (1996)"
+    start = time.clock()
+    print rec_base_item.get_recommendations('Ripe (1996)')[0:30]
     elapsed = (time.clock() - start)
     print elapsed
 
-    recommand_elements.set_similarity_calculator("sim_tanimoto")
-
-    recommand_elements.update_sim_elememts()
+    rec_base_item.set_similarity_calculator("sim_tanimoto")
     start = time.clock()
-    print recommand_elements.getRecommendedElements(prefs, '1633')[0:30]
+    print rec_base_item.get_recommended_items("87", update = True, num = 50)[0:30]
     elapsed = (time.clock() - start)
     print elapsed
 
